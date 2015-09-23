ba0c94b950111c04823cba93ad62da0a62c007c8
datamadic <xavier@openfin.co>
2015-09-23 10:21:20 -0400

    updated log logic

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
-fin.desktop.main(init);
+window.addEventListener('DOMContentLoaded', fin.desktop.main.bind(null, init));
 
 // set the window to be 500 x 500
 function init() {
+    var me = fin.desktop.Window.getCurrent();
 
-    fin.desktop.System.clearCache({
-        appcache: true,
-    }, function() {
-        var me = fin.desktop.Window.getCurrent();
+    me.getBounds(function(bounds) {
+        var desiredHeight = 500,
+            desiredWidth = 500,
+            totalWidth = bounds.width,
+            totalHeight = bounds.height,
+            windowWidthDiff = totalWidth - window.innerWidth,
+            windowWidthHeightDiff = totalHeight - window.innerHeight,
+            newWidth = desiredWidth + windowWidthDiff,
+            newHeight = desiredHeight + windowWidthHeightDiff;
 
-        me.getBounds(function(bounds) {
-            var desiredHeight = 500,
-                desiredWidth = 500,
-                totalWidth = bounds.width,
-                totalHeight = bounds.height,
-                windowWidthDiff = totalWidth - window.innerWidth,
-                windowWidthHeightDiff = totalHeight - window.innerHeight,
-                newWidth = desiredWidth + windowWidthDiff,
-                newHeight = desiredHeight + windowWidthHeightDiff;
-
-            console.log('the window before', window.innerHeight, window.innerWidth);
-
-            me.resizeTo(newWidth, newHeight, "top-left", function() {
-                console.log('the window after', window.innerHeight, window.innerWidth, args);
-                document.write('Window width: ', window.innerWidth, '. Window height: ', window.innerHeight);
-                fin.desktop.Window.getCurrent().show();
-            });
-        });
+        me.resizeTo(newWidth, newHeight, "top-left", function() {
+            document.write('window.innerWidth: ', window.innerWidth, ' window.innerHeight: ', window.innerHeight);
+            me.show();
+        }, me.show);
     });
 }
